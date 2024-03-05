063c04352291f05e5ccdb75fb91d28441c52112d
PhamVanAn190804 <you@example.com>
2024-03-05 22:48:34 +0700

    nopbt ngay 28/2

---
+++ baithuchanh/src/nhanvien.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+import javax.xml.crypto.NoSuchMechanismException;
+
+public class nhanvien {
+    String HoTen;  
+    String GioiTinh;
+    String QueQuan;
+    String PhongBan;
+    String DiaChi;
+    int manv;
+    int NamSinh;
+    int Tuoi;
+    
+    public void nhapThongTinNv(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("nhap Ho va Ten nhan vien : ");
+        HoTen = sc.nextLine();
+        System.out.print("nhap gioi tinh :");
+        GioiTinh = sc.nextLine();
+        System.out.print("nhap que quan nhan vien : ");
+        QueQuan = sc.nextLine();
+        System.out.print("Phong ban hien tai : ");
+        PhongBan = sc.nextLine();
+        System.out.print("Nhap dia chi thuong tru :");
+        DiaChi = sc.nextLine();
+        System.out.print("nhap ma nhan vien : ");
+        manv = sc.nextInt();
+        System.out.print("nhap nam sinh : ");
+        NamSinh = sc.nextInt();
+    }
+    public int TinhTuoi(){
+        Tuoi = 2024 - NamSinh;
+        return Tuoi;
+    }
+    public void inThongTinNv()
+    {
+        System.out.println("Thong tin nhan vien :");
+        System.out.println("Ten: "+HoTen);
+        System.out.println("GioiTinh :" +GioiTinh);
+        System.out.println("Ma so nhan vien: "+manv);
+        System.out.println("Tuoi: "+Tuoi);
+        System.out.println("Que quan: "+QueQuan);
+        System.out.println("Phong ban :" +PhongBan);
+        System.out.println("Dia Chi:" +DiaChi);
+    }
+    public static void main(String[] args) {
+        nhanvien nv = new nhanvien();
+        nv.nhapThongTinNv();
+        nv.TinhTuoi();
+        nv.inThongTinNv();
+    }
+}
+
---
+++ baithuchanh/src/sinhvien.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class sinhvien {
+    public String HoTen;
+    public String QueQuan;
+    public String GioiTinh;
+    public int masv;
+    public int NamSinh;
+    public int Tuoi;
+    public void nhapThongTinSv(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("nhap Ho va Ten sinh vien : ");
+        HoTen = sc.nextLine();
+        System.out.print("nhap que quan sinh vien : ");
+        QueQuan = sc.nextLine();
+        System.out.print("nhap gioi tinh : ");
+        GioiTinh = sc.nextLine();
+        System.out.print("nhap nam sinh : ");
+        NamSinh = sc.nextInt();   
+        System.out.print("nhap ma sinh vien : ");
+        masv = sc.nextInt();
+
+    }
+    public int TinhTuoi(){
+        Tuoi = 2024 - NamSinh;
+        return Tuoi;
+    }
+    public void inThongTinsv()
+    {
+        System.out.println("Thong tin sinh vien :");
+        System.out.println("Ten: "+HoTen);
+        System.out.println("GioiTinh: " +GioiTinh);
+        System.out.println("Ma so sinh vien: "+masv);
+        System.out.println("Tuoi: "+Tuoi);
+        System.out.println("Que quan: "+QueQuan);
+    }
+    public static void main(String[] args) {
+        sinhvien sv = new sinhvien();
+        sv.nhapThongTinSv();
+        sv.TinhTuoi();
+        sv.inThongTinsv();
+
+    }
+}
